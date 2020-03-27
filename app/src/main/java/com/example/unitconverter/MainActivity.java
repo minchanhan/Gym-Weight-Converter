b058c028c2cda5168ea6bc4020924292f22dfee5
minchanhan <minchanhan5@gmail.com>
2020-03-27 01:15:45 -0400

    Added Rate me function to toolbar menu item

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.unitconverter;
 
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -105,19 +108,43 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         String message = getString(R.string.about);
         String message2 = getString(R.string.about2);
-        String message3 = getString(R.string.about3);
-        String message4 = getString(R.string.about4);
-        String finalMessage = message + "\n\n" + message2 + "\n\n" + message3 + "\n\n" + message4;
+        String finalMessage = message + "\n\n" + message2;
 
         switch (item.getItemId()) {
             case R.id.info:
-                AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                alert.setMessage(finalMessage).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                final AlertDialog.Builder thanks = new AlertDialog.Builder(this);
+                thanks.setTitle("Thanks for the help!");
+                thanks.setMessage("Let's return to the app");
+                thanks.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
-                }).setTitle("Thanks for using my app!").create();
+                });
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setTitle("Thanks for using my app!").create();
+                alert.setMessage(finalMessage);
+                alert.setPositiveButton("OK, Rate!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("market://details?id=" + getPackageName())));
+                        } catch (ActivityNotFoundException e) {
+                            startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                        }
+                        thanks.show(); // Need to add some sort of delay
+                    }
+                });
+                alert.setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
                 alert.show();
                 break;
             default:
